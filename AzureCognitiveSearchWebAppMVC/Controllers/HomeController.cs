f7a1b0b6d971d81792c847e42ac8342788d529a0
Guilherme Natal de mello <Gui_Natal@hotmail.com>
2021-07-20 16:56:11 +0100

    first commit (fixed search using azure search)

---
+++ AzureCognitiveSearchWebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
+using AzureCognitiveSearchWebAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCognitiveSearchWebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration Configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string serviceName = Configuration["azure_cognitive:service_name"];
+            string endpoint = $"https://{serviceName}.search.windows.net";
+            string indexName = Configuration["azure_cognitive:index_name"];
+            string apiKey = Configuration["azure_cognitive:api_key"];
+
+            Uri serviceEndpoint = new(endpoint);
+            AzureKeyCredential credential = new(apiKey);
+
+            SearchClient searchClient = new(serviceEndpoint, indexName, credential);
+
+            SearchOptions searchOptions = new();
+            searchOptions.Select.Add("content");
+
+            Response<SearchResults<File>> response = await searchClient.SearchAsync<File>("Broadway", searchOptions);
+
+             return View(response.Value.GetResults());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureCognitiveSearchWebAppMVC/Models/File.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Search.Documents.Indexes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AzureCognitiveSearchWebAppMVC.Models
+{
+    public class File
+    {
+        [SearchableField]
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+}
